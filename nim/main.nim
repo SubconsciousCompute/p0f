54eca718886f9047d511d7d16b920b3fa3d21160
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-05-12 17:26:09 +0530

    temp commit.

--- nim/main.nim
+++ nim/main.nim
@@ -29,28 +29,25 @@ proc handle_query(q: ptr p0f_api_query, r: ptr p0f_api_response) {.header: "api.
 var
   a : p0f_api_query
   b : p0f_api_response
+  socket: string
+  ip: string
 
-var p = initOptParser("--socket")
-while true:
-  p.next()
-  case p.kind
-  of cmdEnd:
-    break
-  of cmdShortOption, cmdLongOption:
-    if p.key == "":
-      echo "Option: ", p.key
-    else:
-      echo "Option and value: ", p.key, ", ", p.val
-  of cmdArgument:
-    echo "Argument: ", p.key
-
-info("Starting client")
-
-a.magic = 0x50304601
-a.addr_type = 4'u8
 
 # Command line arguments.
-a.addr = cast[array[16, uint8]]("192.168.1.5")
+var p = initOptParser()
+p.next()
+socket = p.key
+p.next()
+ip = p.key
+
+doAssert(socket.len > 0 and ip.len > 0, "USAGE: ./p0f-nim-client socket ip")
+
+info("Starting client")
+info("Socket=", socket, " ip=", ip)
+
+a.addr = cast[array[16, uint8]](ip)
+a.magic = 0x50304601
+a.addr_type = 4'u8
 
 # linking error here.
 handle_query(addr a, addr b)
